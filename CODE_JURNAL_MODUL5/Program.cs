a63a44d9ae2db06004edb283fb025540d9184a06
ariel <fadillah0777@gmail.com>
2026-03-25 23:54:40 +0800

    menambahkan class SimpleDataBase

--- CODE_JURNAL_MODUL5/Program.cs
+++ CODE_JURNAL_MODUL5/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿SimpleDataBase<float> db = new SimpleDataBase<float>();
+db.AddNewData(10f);
+db.AddNewData(30f);
+db.AddNewData(82f);
+db.PrintAllData();
+
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+        }
+    }
+}
